c0fbf18159f49cb6ee178014f31137c647a3cac5
rlarag12 <rlarag1297@gmail.com>
2026-01-21 19:31:21 +0000

    ya mero

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -10,15 +10,16 @@ const Home = () => {
 		<NavBar />
 		<Jumbotron />
 		<Cards />
+		<Footer />
 		</div>
 	);
 };
 
 const NavBar = () =>{
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark px-5">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Start Bootstrap</a>
+    <a className="navbar-brand ps-5" href="#">Start Bootstrap</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -27,7 +28,7 @@ const NavBar = () =>{
         <a className="nav-link active" aria-current="page" href="#">Home</a>
         <a className="nav-link" href="#">About</a>
         <a className="nav-link" href="#">Services</a>
-        <a className="nav-link" href="#">Contact</a>
+        <a className="nav-link me-4" href="#">Contact</a>
       </div>
     </div>
   </div>
@@ -37,8 +38,8 @@ const NavBar = () =>{
 
 const Jumbotron = () => {
 	return (
-		<div className="container-xl bg-secondary mb-4">
-			<h1>A Warm Welcome!</h1>
+		<div className="container-xl bg-light rounded mb-4 mt-3 pt-3">
+			<h1 className="display-4">A Warm Welcome!</h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 			<button type="button" className="btn btn-primary mb-5">Call to action!</button>
 		</div>
@@ -50,25 +51,36 @@ const Cards = () =>{
 	for (let i=0; i<4; i++){
 		cardData.push({
 			title: "Card title",
-			image: "https://via.placeholder.com/300x200",
-			text: "Some quick example text to build on the card title and make up the bulk of the card’s content."
+			image: "https://placehold.co/500x325",
+			text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 		})
 	}
 	return (
 	<div className="container">
-		<div className="row">
-		{cardData.map((card, index)=>(
-		<div className="card me-3" style={{ width: "18rem" }} key={index}>
-  <img src={card.image} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{card.title}</h5>
-    <p className="card-text">{card.text}</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-		))}
+		<div className="row text-center">
+		  {cardData.map((card, index)=>(
+			<div className="col-12 col-md-3 mb-3" key={index}>
+		<div className="card me-3 text-center" style={{ width: "18rem" }} key={index}>
+            <img src={card.image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+               <h5 className="card-title">{card.title}</h5>
+               <p className="card-text">{card.text}</p>
+			   <hr className="w-100"></hr>
+               <a href="#" className="btn btn-primary">Finde Out More!</a>
+            </div>
+        </div>
+		</div>
+		  ))}
 		</div>
 	</div>
+	);
+};
+
+const Footer = () =>{
+	return (
+		<div className="container-fluid bg-dark py-4 mt-3">
+			<p className="text-light text-center">Copyright Your Website 2026</p>
+		</div>
 	);
 };
 
